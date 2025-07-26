a49e8d658e4d52d8a8ba3513a56d1c963e1a6f33
Your Name <you@example.com>
2025-07-26 17:29:50 +0300

    create cubit

---
+++ lib/cubits/add_notes_cubit/add_notes_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_notes_cubit/add_notes_state.dart';
+
+class AddNotesCubit extends Cubit<NotesState>
+{
+  AddNotesCubit(super.initialState);
+
+}
