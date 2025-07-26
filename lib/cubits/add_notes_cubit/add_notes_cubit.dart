59fdee2c01fae48571a6be1df9fff19b00567654
Your Name <you@example.com>
2025-07-26 17:38:34 +0300

    add note state

--- lib/cubits/add_notes_cubit/add_notes_cubit.dart
+++ lib/cubits/add_notes_cubit/add_notes_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/add_notes_cubit/add_notes_state.dart';
 
-class AddNotesCubit extends Cubit<NotesState>
+class AddNotesCubit extends Cubit<AddNotesState>
 {
   AddNotesCubit(super.initialState);
 
