e119d4935a21ad9b397b52fbc656c2ed9826517e
Your Name <you@example.com>
2025-07-26 17:55:45 +0300

    register adapter

--- lib/cubits/add_notes_cubit/add_notes_cubit.dart
+++ lib/cubits/add_notes_cubit/add_notes_cubit.dart
@@ -1,12 +1,14 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/add_notes_cubit/add_notes_state.dart';
+import 'package:notes_app/models/note_model.dart';
 
-<<<<<<< HEAD
 class AddNotesCubit extends Cubit<AddNotesState>
-=======
-class AddNotesCubit extends Cubit<NotesState>
->>>>>>> a49e8d658e4d52d8a8ba3513a56d1c963e1a6f33
 {
   AddNotesCubit(super.initialState);
+  
 
+  addNotes (NoteModel note)
+  {
+    
+  }
 }
---
+++ lib/cubits/add_notes_cubit/add_notes_state.dart
@@ -0,0 +1,11 @@
+abstract class AddNotesState {}
+class AddNotesIntial extends AddNotesState{}
+class AddNotesLoading extends AddNotesState{}
+class AddNotesSuccess extends AddNotesState{}
+class AddNotesFailer extends AddNotesState{
+  final String errmessage ;
+
+  AddNotesFailer({required this.errmessage}); 
+}
+
+
