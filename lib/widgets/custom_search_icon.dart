412c7a247e2f169f6e1dbe1fd0f1facf9185683a
Your Name <you@example.com>
2025-08-10 21:29:50 +0300

    edit note done

--- lib/widgets/custom_search_icon.dart
+++ lib/widgets/custom_search_icon.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class CustomSearchIcon extends StatelessWidget {
   const CustomSearchIcon
-({super.key, required this.icon});
+({super.key, required this.icon , this.onPressed});
  final IconData icon;
-
+ final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -15,8 +15,11 @@ class CustomSearchIcon extends StatelessWidget {
         borderRadius:BorderRadius.circular(16)    
       ),
       child: Center(
-        child: Icon(icon,
+        child: IconButton(
+        onPressed: onPressed,
+        icon: Icon(icon,
         size: 28,
+        ) ,
         ),
       ),
     );
